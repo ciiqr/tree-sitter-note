20de823e0524b8502e3353a03c81530ccf47a104
William Villeneuve <ciiqr@users.noreply.github.com>
2026-04-21 18:33:11 -0400

    added note specific highlight captures

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,12 +1,15 @@
-; TODO: can likely suffix these (ie. give each a unique name so themes can style notes specifically, was using .note in sublime)
-(comment) @comment
-(section) @string
-(done) @function
-(todo) @variable.parameter
-(question) @function.special
-(partial) @constant
-(important) @keyword
-(urgent) @invalid
+; DOCS: https://neovim.io/doc/user/treesitter/#treesitter-highlight-groups
+(comment)   @comment @note.comment
+(section)   @string @note.section
+(done)      @function @note.done
+(todo)      @variable.parameter @note.todo
+; TODO: why @function.special over @constant again?
+(question)  @function.special @note.question
+(partial)   @constant @note.partial
+(important) @keyword @note.important
+(urgent)    @invalid @note.urgent
+; TODO: consider alternatives to @invalid (preference may be different per-editor)
+; - @warning @markup.strong
 
 ; code block
 (code_fence_start) @punctuation.delimiter
